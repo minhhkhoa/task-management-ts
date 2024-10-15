96cce133571f7a2824d39e90d2b0bc0eec7b3af3
minhhkhoa <khoanguyen.1881@gmail.com>
2024-10-15 22:14:00 +0700

    9. Cài đặt CORS

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express} from "express"
 import * as database from './config/database'
 import dotenv from 'dotenv'
 import mainV1Routes from "./api/v1/routes/index.route"
+import cors from "cors"
 
 dotenv.config()
 
@@ -9,6 +10,8 @@ database.connect()
 
 const app: Express = express()
 const port: number | string = process.env.PORT || 3000
+
+app.use(cors())
 
 //-thay the cho body-parser
 app.use(express.json()); // Để parse JSON
